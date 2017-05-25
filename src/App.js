edea51bca62823d4c6cb6cf565d92a4cf313c4f6
aurinely lopez <aurinely@gmail.com>
2017-05-25 18:47:42 -0400

    this that and the other

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import Baoquestion from './components/Baoquestion';
 //importing question component
 import quiztime from './api/quiztime';
+import Baoscore from './components/Baoscore';
+import answerChoices from './components/answerchoices';
+
 
 class App extends Component {
 
@@ -17,6 +20,7 @@ class App extends Component {
       answer: '',
       answersCount: {
         User: 0,
+
       },
       result: ''
     };
@@ -24,16 +28,15 @@ class App extends Component {
     this.handleAnswerSelected = this.handleAnswerSelected.bind(this);
   }
 
-componentWillMount() {
-const shuffledAnswerChoices = quizTime.map((question) => this.shuffleArray(question.answers));  
-
-this.setState({
-question: quizTime[0].question,
-answerChoices: shuffledAnswerChoices[0]
+  componentWillMount() {
+    const shuffledAnswerChoices = quiztime.map((question) => this.shuffleArray(question.answers));
+    this.setState({
+      question: quiztime[0].question,
+      answerChoices: shuffledAnswerChoices[0]
     });
   }
 
-shuffleArray(array) {
+  shuffleArray(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
     // While there remain elements to shuffle...
@@ -51,15 +54,147 @@ shuffleArray(array) {
 
     return array;
   };
-  render() {
-    render (
-    <div className="App">
-      <div className="App-header">
-        <h2>BBBAO!!</h2>
-      </div>
-      <Question content="How old am I?" />
-    </div>
-  );
+
+  handleAnswerSelected(event) {
+    this.setUserAnswer(event.currentTarget.value);
+
+    if (this.state.questionId < quiztime.length) {
+        setTimeout(() => this.setNextQuestion(), 300);
+    } else {
+        setTimeout(() => this.setResults(this.getResults()), 300);
+    }
   }
+
+  setUserAnswer(answer) {
+    const updatedAnswersCount = update(this.state.answersCount, {
+      [answer]: {$apply: (currentValue) => currentValue + 1}
+    });
+
+    this.setState({
+        answersCount: updatedAnswersCount,
+        answer: answer
+    });
+  }
+
+  setNextQuestion() {
+    const counter = this.state.counter + 1;
+    const questionId = this.state.questionId + 1;
+
+    this.setState({
+        counter: counter,
+        questionId: questionId,
+        question: quiztime[counter].question,
+        answerOptions: quiztime[counter].answers,
+        answer: ''
+    });
+  }
+
+  getResults() {
+    const answersCount = this.state.answersCount;
+    const answersCountKeys = Object.keys(answersCount);
+    const answersCountValues = answersCountKeys.map((key) => answersCount[key]);
+    const maxAnswerCount = Math.max.apply(null, answersCountValues);
+
+    return answersCountKeys.filter((key) => answersCount[key] === maxAnswerCount);
+  }
+
+  setResults(result) {
+    if (result.length === 1) {
+      this.setState({ result: result[0] });
+    } else {
+      this.setState({ result: 'Undetermined' });
+    }
+  }
+
+  renderTest() {
+    return (
+      <test
+        answer={this.state.answer}
+        answerOptions={this.state.answerOptions}
+        questionId={this.state.questionId}
+        question={this.state.question}
+        questionTotal={answerChoices.length}
+        onAnswerSelected={this.handleAnswerSelected}
+      />
+    );
+  }
+
+  renderResult() {
+    return (
+      <Result quizResult={this.state.result} />
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>React Quiz</h2>
+        </div>
+        {this.state.result ? this.renderResult() : this.renderQuiz()}
+      </div>
+    );
+  }
+
 }
+
 export default App;
+// class App extends Component {
+
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       counter: 0,
+//       questionId: 1,
+//       question: '',
+//       answerOptions: [],
+//       answer: '',
+//       answersCount: {
+//         User: 0,
+//       },
+//       result: ''
+//     };
+
+//     this.handleAnswerSelected = this.handleAnswerSelected.bind(this);
+//   }
+
+// componentWillMount() {
+// const shuffledAnswerChoices = quizTime.map((question) => this.shuffleArray(question.answers));  
+
+// this.setState({
+// question: quizTime[0].question,
+// answerChoices: shuffledAnswerChoices[0]
+//     });
+//   }
+
+// shuffleArray(array) {
+//     var currentIndex = array.length, temporaryValue, randomIndex;
+
+//     // While there remain elements to shuffle...
+//     while (0 !== currentIndex) {
+
+//       // Pick a remaining element...
+//       randomIndex = Math.floor(Math.random() * currentIndex);
+//       currentIndex -= 1;
+
+//       // And swap it with the current element.
+//       temporaryValue = array[currentIndex];
+//       array[currentIndex] = array[randomIndex];
+//       array[randomIndex] = temporaryValue;
+//     }
+
+//     return array;
+//   };
+//   render() {
+//     render (
+//     <div className="App">
+//       <div className="App-header">
+//         <h2>BBBAO!!</h2>
+//       </div>
+//       <Question content="How old am I?" />
+//     </div>
+//   );
+//   }
+// }
+// export default App;
